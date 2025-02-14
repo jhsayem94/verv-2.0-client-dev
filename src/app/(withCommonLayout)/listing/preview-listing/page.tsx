bb6106c34165043816d59c65ab6d16ee88dff7fb
aminul <rahathssn@gmail.com>
2025-02-14 18:34:34 +0600

    added property details to the preview page

--- src/app/(withCommonLayout)/listing/preview-listing/page.tsx
+++ src/app/(withCommonLayout)/listing/preview-listing/page.tsx
@@ -1,6 +1,20 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+
+import { usePropertyDetailsStore } from "@/store/store";
 import ListingTab from "../../components/Shared/Tabs/ListingTab";
 
 const page = () => {
+  const propertyDetails = usePropertyDetailsStore(
+    (state) => state.propertyDetails
+  );
+  const tenancyDetails = usePropertyDetailsStore(
+    (state) => state.tenancyDetails
+  );
+
+  console.log(propertyDetails);
+  console.log(tenancyDetails);
+
   return (
     <div>
       <ListingTab isPreview={true} />
