277a04fc686be38ff8d16c702f56c5de7d0d2d3c
aminul <rahathssn@gmail.com>
2025-02-24 19:50:27 +0600

    added register process

--- src/app/(withCommonLayout)/components/pages/Register/Register.tsx
+++ src/app/(withCommonLayout)/components/pages/Register/Register.tsx
@@ -17,21 +17,16 @@ const Register = () => {
     formState: { errors },
   } = useForm<TRegister>({
     resolver: zodResolver(registerSchema),
-    // defaultValues: { propertyOption: "Whole property" },
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
 
-  //   className="flex w-full items-start self-stretch px-3 py-1.5 rounded-md border border-gray-400 bg-white pr-10 mb-4"
-  // pass  className="flex w-full items-start self-stretch px-3 py-1.5 rounded-md border border-gray-400 bg-white pr-10"
-
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="w-full">
       <InputField
         registerName="email"
-        //   label="Email"
         type="email"
         placeholder="Email"
         register={register}
@@ -44,24 +39,41 @@ const Register = () => {
         placeholder="Password"
         register={register}
         errors={errors}
-        className="w-full"
+        className="w-full mt-4 mb-6"
       />
 
-      {/* <PasswordInputField
-        register={register}
-        errors={errors}
-        className="flex w-full items-start self-stretch px-3 py-1.5 rounded-md border border-gray-400 bg-white pr-10"
-      /> */}
-
-      {/* <div className="w-full flex justify-between items-center gap-6 mt-6">
-        <p className="text-colorTextPrimary leading-[150%]">Register as</p>
-        <RadioGroupField
-          options={registerOptions}
-          registerAs="role"
-          register={register}
-          errors={errors}
-        />
-      </div> */}
+      <div className="w-full">
+        <div className="flex items-center gap-6">
+          <p className="text-colorTextPrimary leading-[150%]">Register as</p>
+          <div className="flex items-center gap-4">
+            <div className="flex justify-center items-center gap-2">
+              <input
+                type="radio"
+                value="landlord"
+                {...register("role")}
+                className="w-[18px] h-[18px] accent-green-600"
+              />
+              <p className="text-sm text-colorTextPrimary font-semibold">
+                Landlord/Agent
+              </p>
+            </div>
+            <div className="flex justify-center items-center gap-2">
+              <input
+                type="radio"
+                value="tenant"
+                {...register("role")}
+                className="w-[18px] h-[18px] accent-green-600"
+              />
+              <p className="text-sm text-colorTextPrimary font-semibold">
+                Tenant
+              </p>
+            </div>
+          </div>
+        </div>
+        {errors.role && (
+          <p className="text-red-500 text-sm">{errors.role.message}</p>
+        )}
+      </div>
       <Button
         type="submit"
         className="bg-colorButton w-full mt-6 rounded shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] text-lg font-semibold"
