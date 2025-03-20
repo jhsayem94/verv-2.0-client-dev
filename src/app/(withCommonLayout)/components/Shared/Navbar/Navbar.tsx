c8530223f5078f9930841a89593ea328b8cd73ec
aminul <rahathssn@gmail.com>
2025-03-20 19:34:23 +0600

    added changed navbar

--- src/app/(withCommonLayout)/components/Shared/Navbar/Navbar.tsx
+++ src/app/(withCommonLayout)/components/Shared/Navbar/Navbar.tsx
@@ -7,12 +7,18 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import logo from "../../../../../assets/images/logo.png";
+import logo from "../../../../../assets/images/logo-white.png";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { CirclePerson, DownArrow, HomeIcon } from "@/assets/icons/icons";
+import {
+  CirclePerson,
+  DownArrow,
+  HomeIcon,
+  NavFavourite,
+  Notification,
+} from "@/assets/icons/icons";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/context/user.provider";
 import { logout } from "@/services/AuthServices";
@@ -34,13 +40,13 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="py-6 bg-white shadow-[0px_4px_4px_rgba(0,0,0,0.10)] hidden md:block sticky top-0 z-20">
-      <div className="w-[1216px] m-auto flex justify-between items-center text-[#314660]">
-        <Link href="/">
-          <Image src={logo} width={137.951} alt="verv logo" />
-        </Link>
-        <div className="text-lg flex justify-end items-center gap-14">
-          <div className="font-medium">
+    <nav className="px-10 py-6 bg-colorButton shadow-[0px_4px_4px_rgba(0,0,0,0.10)] hidden md:block sticky top-0 z-20">
+      <div className="m-auto flex justify-between items-center text-white">
+        <div className="flex items-center gap-20">
+          <Link href="/">
+            <Image src={logo} width={137.951} alt="verv logo" />
+          </Link>
+          <div className="font-medium flex items-center gap-6">
             <ul className="flex justify-between items-center gap-6">
               <li>
                 <DropdownMenu>
@@ -90,7 +96,17 @@ const Navbar = () => {
               </li>
               <li className="flex justify-center items-center">Agent Finder</li>
             </ul>
+            <Button
+              className="text-lg text-colorButton font-semibold bg-white rounded-[32px] w-[180px] h-[56px] py-2 px-4"
+              onClick={() => router.push("/listing/add-property")}
+            >
+              <HomeIcon width={25} height={24} />
+              Add Listing
+            </Button>
           </div>
+        </div>
+        <div className="text-lg flex justify-end items-center gap-14">
+          {/* signin/register */}
           <div className="flex justify-end items-center gap-6 font-medium">
             <ul className="flex items-center justify-center gap-2">
               {!user ? (
@@ -104,37 +120,38 @@ const Navbar = () => {
                   </Link>
                 </li>
               ) : (
-                <li>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Avatar className="cursor-pointer">
-                        <AvatarImage
-                          src="https://github.com/shadcn.png"
-                          alt="@shadcn"
-                        />
-                        <AvatarFallback>CN</AvatarFallback>
-                      </Avatar>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-36">
-                      <DropdownMenuGroup>
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Setting</DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => handleLogout()}>
-                          Logout
-                        </DropdownMenuItem>
-                      </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </li>
+                <div className="flex items-center gap-6">
+                  <li>
+                    <NavFavourite height={28} width={28} stroke="white" />
+                  </li>
+                  <li>
+                    <Notification height={28} width={28} fill="white" />
+                  </li>
+                  <li>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Avatar className="cursor-pointer w-12 h-12">
+                          <AvatarImage
+                            src="https://avatar.iran.liara.run/public"
+                            alt="avatar"
+                          />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="w-36">
+                        <DropdownMenuGroup>
+                          <DropdownMenuItem>Profile</DropdownMenuItem>
+                          <DropdownMenuItem>Setting</DropdownMenuItem>
+                          <DropdownMenuItem onClick={() => handleLogout()}>
+                            Logout
+                          </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </li>
+                </div>
               )}
             </ul>
-            <Button
-              className="text-lg font-semibold rounded-[32px] bg-[#50B533] w-[180px] h-[56px] py-2 px-4"
-              onClick={() => router.push("/listing/add-property")}
-            >
-              <HomeIcon width={25} height={24} />
-              Add Listing
-            </Button>
           </div>
         </div>
       </div>
